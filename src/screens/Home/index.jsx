64490f479a64c12be5c62ac00c204c309a803e5b
GuiHJLima <guilhermehenriquelima.564@gmail.com>
2024-08-28 12:56:52 -0300

    feat: create screan navigation

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Home() {
+  const navigation = useNavigation();
   return (
     <View>
       <Text>Home Screen</Text>
-      
+      <Text>Click on the button below to go to the Totoro screen</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Totoro')}>
+        <Text>Go to Totoro</Text>
+      </TouchableOpacity>
     </View>
   )
 }
--- src/screens/Totoro/index.jsx
+++ src/screens/Totoro/index.jsx
@@ -1,9 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, TouchableOpacity } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 export default function Totoro() {
+
+  const navigation = useNavigation()
+  
   return (
     <View>
       <Text>Totorohaus</Text>
+      <ImageBackground source={require('./../../../assets/totoro.jpg')} style={{ width: '100%', height: '70%' }} />
+      <Text>Click on the button below to go to the Home screen</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+        <Text>Go to Home</Text>
+      </TouchableOpacity>
     </View>
   )
 }
