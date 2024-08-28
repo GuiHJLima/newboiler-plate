8ba91651257ce9148833182d53e98507181ee065
GuiHJLima <guilhermehenriquelima.564@gmail.com>
2024-08-28 08:52:08 -0300

    chore: add react navegation lib

--- App.jsx
+++ App.jsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>tudo bão</Text>
+      <Image style={styles.image} source={{uri: 'https://pm1.aminoapps.com/7756/7ab02fe074250fcb0540fb9dae7247f6dbfbb8a4r1-389-389v2_00.jpg' }} />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  image: {
+    width: 200,
+    height: 200,
+  }
 });
