bbd581031ff8ed59d2f564b6d885432685f9aeaa
GuiHJLima <guilhermehenriquelima.564@gmail.com>
2024-08-28 16:25:49 -0300

    finished

--- src/screens/Ghibli/index.jsx
+++ src/screens/Ghibli/index.jsx
@@ -1,17 +1,49 @@
-import { View, Text} from 'react-native';
-
+import { View, Text, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import { useState } from 'react';
 import styles from './styles';
 import Title from '../../components/Title';
 import MyButton from '../../components/Button';
 
 export default function Ghibli() {
+  const [movieName, setMovieName] = useState("");
+  const [protagonist, setProtagonist] = useState("");
+
+  const cadastrar = () => {
+    console.warn("Movie Name: ", movieName);
+    console.warn("Protagonist: ", protagonist);
+  }
   return (
     <View style={styles.container}>
+      <ScrollView>
       <Title title="Welcome to the Ghibli screen" />
+      
+      <View style={styles.textInputContainer}>
+        <Text style={styles.text}>Insert a Movie here:</Text>
+        <TextInput style={styles.textInput} 
+        placeholder="Movie Name:" 
+        value={movieName}
+        onChangeText={setMovieName}
+        />
+              <Text style={styles.text}>Insert a Protagonist here:</Text>
+        <TextInput style={styles.textInput} 
+        placeholder="Protagonist Name:" 
+        value={protagonist}
+        onChangeText={setProtagonist}
+        />
+
+        <TouchableOpacity style={styles.sendButton}
+        onPress={cadastrar}
+        >
+          <Text style={styles.textButton}>Click me 😎😎😎😎</Text>
+        </TouchableOpacity>
+      </View>
+
       <Text style={styles.text}>Click on the button below to go to the Totoro screen</Text>
-      <MyButton destination="Totoro" name="Totoro"/>
+      <MyButton destination="Totoro" name="Totoro" />
+
       <Text style={styles.text}>Click on the button below to go to the Home screen</Text>
-      <MyButton destination="Home" name="Home" type={"back"}/>
+      <MyButton destination="Home" name="Home" type={"back"} />
+      </ScrollView>
     </View>
   )
 }
--- src/screens/Ghibli/styles.js
+++ src/screens/Ghibli/styles.js
@@ -10,6 +10,31 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 20,
   },
+
+  textInput: {
+    height: 40,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 20,
+  },
+  sendButton: {
+    backgroundColor: 'green',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  textButton:{
+    fontSize: 20,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    padding: 10,
+  },
+  textInputContainer: {
+    alignItems: 'center',
+    marginBottom: 30,
+    marginTop: 20,
+  },
 });
 
 export default styles;
